dd9c159cc9d4d510e2aa121ed511f29ff22a590e
Omyadav7354 <omyadav7354@gmail.com>
2025-01-28 18:10:10 +0530

    first commit

--- model/User.js
+++ model/User.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
     name: String,
-    mobile: Number,
+    mobile: {
+        type: Number,
+        unique: true,
+        required: true,
+        minlength: 10,
+        maxlength: 10,        
+    },
     email: String,
     password: String,
 })
