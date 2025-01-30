7eda400c9640889b4c68e1b0ea500ecf6c9c51ab
Jay-jat <jayjat0547@gmail.com>
2025-01-30 14:40:51 +0530

    jay

--- model/Blog.js
+++ model/Blog.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const BlogSchema = new mongoose.Schema({
     title: String,
     description: String,
-    image: String
+    images: String
 })
 
 module.exports = mongoose.model('Blog', BlogSchema)
